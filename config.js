baae9e41b83e47e0d1dd3545b0d0df1dcfbbd559
Sayantan Chakraborty <67456004+rickdgamer3@users.noreply.github.com>
2022-03-15 18:51:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,124 +6,144 @@
 // └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
 
 const CONFIG = {
-    // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
-    // ├┴┐├─┤└─┐││  └─┐
-    // └─┘┴ ┴└─┘┴└─┘└─┘
-  
-    // General
-    name: 'Sayantan',
-    imageBackground: false,
-    openInNewTab: true,
-    twelveHourFormat: true,
-  
-    // Greetings
-    greetingMorning: 'Good morning!',
-    greetingAfternoon: 'Good afternoon,',
-    greetingEvening: 'Good evening,',
-    greetingNight: 'Go to Sleep!',
-  
-    // Layout
-    bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
-  
-    // Weather
-    weatherKey: 'api.openweathermap.org/data/2.5/weather?lat={22.572645}&lon={88.363892}&appid={API key}', // Write here your API Key
-    weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
-    weatherUnit: 'C', // 'F', 'C'
-    language: 'en', // More languages in https://openweathermap.org/current#multi
-  
-    trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-    defaultLatitude: '22.572645',
-    defaultLongitude: '88.363892',
-  
-    // Autochange
-    autoChangeTheme: true,
-  
-    // Autochange by OS
-    changeThemeByOS: true,
-  
-    // Autochange by hour options (24hrs format, string must be in: hh:mm)
-    changeThemeByHour: false,
-    hourDarkThemeActive: '18:30',
-    hourDarkThemeInactive: '07:00',
-  
-    // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
-    // ├┴┐│ │ │  │ │ ││││└─┐
-    // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
-  
-    firstButtonsContainer: [
-      {
-        id: '1',
-        name: 'PW',
-        icon: 'graduation-cap',
-        link: 'https://physicswallah.live/tabs/tabs/batch-tab/6133adb6beb299006d0a9fb8/new-overview',
-      },
-      {
-        id: '2',
-        name: 'Mail',
-        icon: 'mail',
-        link: 'https://mail.google.com/mail/',
-      },
-      {
-        id: '3',
-        name: 'Search',
-        icon: 'trello',
-        link: 'https://todoist.com',
-      },
-      {
-        id: '4',
-        name: 'Whatsapp',
-        icon: 'message-circle',
-        link: 'https://web.whatsapp.com/',
-      },
-      {
-        id: '5',
-        name: 'Reddit',
-        icon: 'glasses',
-        link: 'https://reddit.com',
-      },
-      {
-        id: '6',
-        name: 'Youtube',
-        icon: 'youtube',
-        link: 'https://www.youtube.com/',
-      },
-      {
-        id: '7',
-        name: 'Spotify',
-        icon: 'headphones',
-        link: 'https://open.spotify.com',
-      },
-      {
-        id: '8',
-        name: 'Linkedin',
-        icon: 'linkedin',
-        link: 'https://in.linkedin.com',
-      },
-      {
-        id: '9',
-        name: 'Discord',
-        icon: 'bot',
-        link: 'https://discord.com/app',
-      },
-      {
-        id: '10',
-        name: 'Amazon',
-        icon: 'shopping-bag',
-        link: 'https://amazon.com/',
-      },
-      {
-        id: '11',
-        name: 'Twitche',
-        icon: 'twitch',
-        link: 'https://www.twitch.tv/',
-      },
-      {
-        id: '12',
-        name: 'Telegram',
-        icon: 'send',
-        link: 'https://web.telegram.org/',
-      }
-      // First Links Container
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: 'Sayantan',
+  imageBackground: false,
+  openInNewTab: true,
+  twelveHourFormat: true,
+
+  // Greetings
+  greetingMorning: 'Good morning!',
+  greetingAfternoon: 'Good afternoon,',
+  greetingEvening: 'Good evening,',
+  greetingNight: 'Go to Sleep!',
+
+  // Layout
+  bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
+
+  // Weather
+  weatherKey: 'api.openweathermap.org/data/2.5/weather?lat={22.572645}&lon={88.363892}&appid={API key}'', // Write here your API Key
+  weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: 'C', // 'F', 'C'
+  language: 'en', // More languages in https://openweathermap.org/current#multi
+
+  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: '22.572645',
+  defaultLongitude: '88.363892',
+
+  // Autochange
+  autoChangeTheme: true,
+
+  // Autochange by OS
+  changeThemeByOS: true,
+
+  // Autochange by hour options (24hrs format, string must be in: hh:mm)
+  changeThemeByHour: false,
+  hourDarkThemeActive: '18:30',
+  hourDarkThemeInactive: '07:00',
+
+  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+  // ├┴┐│ │ │  │ │ ││││└─┐
+  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+  firstButtonsContainer: [
+    {
+      id: '1',
+      name: 'Mail',
+      icon: 'mail',
+      link: 'https://mail.google.com/mail/',
+    },
+    {
+      id: '2',
+      name: 'PW',
+      icon: 'graduation-cap',
+      link: 'https://physicswallah.live/tabs/tabs/batch-tab/6133adb6beb299006d0a9fb8/new-overview',
+    },
+    {
+      id: '3',
+      name: 'Todoist',
+      icon: 'trello',
+      link: 'https://todoist.com',
+    },
+    {
+      id: '4',
+      name: 'Whatsapp',
+      icon: 'message-circle',
+      link: 'https://web.whatsapp.com/',
+    },
+    {
+      id: '5',
+      name: 'Reddit',
+      icon: 'glasses',
+      link: 'https://reddit.com',
+    },
+    {
+      id: '6',
+      name: 'Youtube',
+      icon: 'youtube',
+      link: 'https://www.youtube.com/',
+    },
+    {
+      id: '8',
+      name: 'Spotify',
+      icon: 'headphones',
+      link:'https://open.spotify.com',
+    },
+    {
+      id:'9',
+      name: 'Dsicord',
+      icon:'bot',
+      link:'https://discord.com/app',
+  ],
+
+  secondButtonsContainer: [
+    {
+      id: '1',
+      name: 'Music',
+      icon: 'headphones',
+      link: 'https://open.spotify.com',
+    },
+    {
+      id: '2',
+      name: 'twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/',
+    },
+    {
+      id: '3',
+      name: 'bot',
+      icon: 'bot',
+      link: 'https://discord.com/app',
+    },
+    {
+      id: '4',
+      name: 'Amazon',
+      icon: 'shopping-bag',
+      link: 'https://amazon.com/',
+    },
+    {
+      id: '5',
+      name: 'Hashnode',
+      icon: 'pen-tool',
+      link: 'https://hashnode.com/',
+    },
+    {
+      id: '6',
+      name: 'Figma',
+      icon: 'figma',
+      link: 'https://figma.com/',
+    },
+  ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  // First Links Container
   firstlistsContainer: [
     {
       icon: 'music',
@@ -219,4 +239,3 @@ const CONFIG = {
     },
   ],
 };
-    
